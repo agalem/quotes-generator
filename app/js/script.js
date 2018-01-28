aaa946ef8f6adc5bc677153441186a5276f013be
Agnieszka Lempaszek <agnieszka.lempaszek@student.uj.edu.pl>
2018-01-28 13:34:39 +0100

    Random quotes generator

---
+++ app/js/script.js
@@ -0,0 +1,83 @@
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+
+
+//Function which allows changing color
+//source: https://stackoverflow.com/questions/190560/jquery-animate-backgroundcolor
+(function (d) {
+    d.each(["backgroundColor"], function (f, e) {
+        d.fx.step[e] = function (g) {
+            if (!g.colorInit) {
+                g.start = c(g.elem, e);
+                g.end = b(g.end);
+                g.colorInit = true
+            }
+            g.elem.style[e] = "rgb(" + [Math.max(Math.min(parseInt((g.pos * (g.end[0] - g.start[0])) + g.start[0]), 255), 0), Math.max(Math.min(parseInt((g.pos * (g.end[1] - g.start[1])) + g.start[1]), 255), 0), Math.max(Math.min(parseInt((g.pos * (g.end[2] - g.start[2])) + g.start[2]), 255), 0)].join(",") + ")"
+        }
+    });
+
+    function b(f) {
+        var e;
+        if (f && f.constructor == Array && f.length == 3) {
+            return f
+        }
+        if (e = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(f)) {
+            return [parseInt(e[1]), parseInt(e[2]), parseInt(e[3])]
+        }
+        if (e = /rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(f)) {
+            return [parseFloat(e[1]) * 2.55, parseFloat(e[2]) * 2.55, parseFloat(e[3]) * 2.55]
+        }
+        if (e = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(f)) {
+            return [parseInt(e[1], 16), parseInt(e[2], 16), parseInt(e[3], 16)]
+        }
+        if (e = /#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/.exec(f)) {
+            return [parseInt(e[1] + e[1], 16), parseInt(e[2] + e[2], 16), parseInt(e[3] + e[3], 16)]
+        }
+    }
+    function c(g, e) {
+        var f;
+        do {
+            f = d.css(g, e);
+            if (f != "" && f != "transparent" || d.nodeName(g, "body")) {
+                break
+            }
+            e = "backgroundColor"
+        } while (g = g.parentNode);
+        return b(f)
+    }
+})(jQuery);
+
+
+//My function which prints out random quote
+
+function getQuote() {
+    $.ajax({
+        url: '../quotes.json',
+        dataType: 'json'
+    }).done(function (data) {
+        var quotes = data;
+        var number = Math.floor(Math.random() * (quotes.length + 1));
+
+        $('.quote-container').animate({
+            opacity: 0
+        }, 500, function () {
+            $(this).animate({
+                opacity: 1
+            }, 500);
+            $('#quote').text(quotes[number].quoteText);
+            $('#author').text(quotes[number].quoteAuthor);
+        });
+
+        var color = Math.floor(Math.random() * colors.length);
+
+        $('#main').animate({
+            backgroundColor: colors[color]
+        }, 1000);
+        $('.button').animate({
+            backgroundColor: colors[color]
+        }, 1000);
+    })
+}
+
+$('.button').on('click', function () {
+    getQuote();
+});
